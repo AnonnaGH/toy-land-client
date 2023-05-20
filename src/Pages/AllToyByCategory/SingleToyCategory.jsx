02c42d6b69d97e161b360a92951b5f1e549719b6
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-05-20 13:48:43 +0600

    tab catagory added

---
+++ src/Pages/AllToyByCategory/SingleToyCategory.jsx
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from 'sweetalert2'
+
+
+const SingleToyCategory = ({ singleToyCategory }) => {
+
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const { _id, ToyName, SubCategory, Price, AvailableQuantity, Ratings, SellerName, Pictureurl, Details, email } = singleToyCategory;
+
+  const handleViewDetails = () => {
+    if (user) {
+      navigate(`/toys/${_id}`);
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please log in first to see details!',
+      }).then(() => {
+        navigate("/login");
+      });
+    }
+  };
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-xl">
+        <figure>
+          <img className="card-image" src={Pictureurl} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{ToyName}</h2>
+          <p>
+            Price: <span>$ {Price}</span>
+          </p>
+          <div className="card-actions justify-end">
+
+            <Link to={`/toys/${_id}`}> <button onClick={handleViewDetails} className="btn btn-primary">View Details</button></Link>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleToyCategory;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AllToyCategory from "../../AllToyByCategory/AllToyCategory";
 import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AllToyCategory></AllToyCategory>
             <Gallery></Gallery>
         </div>
     );
