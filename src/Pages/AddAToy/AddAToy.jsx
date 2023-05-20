6cb05a0de123d3dbef92967d41813f62bee928c0
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-05-20 10:39:45 +0600

    add a toy ui done

--- src/Pages/AddAToy/AddAToy.jsx
+++ src/Pages/AddAToy/AddAToy.jsx
@@ -1,146 +1,179 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
+
 
 const AddAToy = () => {
-    const [toyData, setToyData] = useState({
-        pictureUrl: '',
-        name: '',
-        sellerName: '',
-        sellerEmail: '',
-        subCategory: '',
-        price: '',
-        rating: '',
-        availableQuantity: '',
-        description: ''
-    });
+    const { user } = useContext(AuthContext);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors },
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setToyData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
+    } = useForm();
+    const onSubmit = (toy) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission logic here
-        console.log(toyData);
-    };
+        // fetch('https://toy-car-zone-server.vercel.app/toys', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(toy)
+        // })
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         alert("Toy added successfully!");
+        //         reset();
 
+        //     })
+
+        console.log(toy);
+
+    }
     return (
-        <div className="container mx-auto p-8">
-            <h1 className="text-2xl font-bold mb-4">Add A Toy</h1>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="pictureUrl">Picture URL of the toy:</label>
-                        <input
-                            type="text"
-                            id="pictureUrl"
-                            name="pictureUrl"
-                            value={toyData.pictureUrl}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
+        <div className=" bg-[#E8F6F7]">
 
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={toyData.name}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
 
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="sellerName">Seller Name:</label>
-                        <input
-                            type="text"
-                            id="sellerName"
-                            name="sellerName"
-                            value={toyData.sellerName}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
 
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="sellerEmail">Seller Email:</label>
-                        <input
-                            type="email"
-                            id="sellerEmail"
-                            name="sellerEmail"
-                            value={toyData.sellerEmail}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
-                    </div>
-
-                    <div>
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="subCategory">Sub-category:</label>
-                        <select
-                            id="subCategory"
-                            name="subCategory"
-                            value={toyData.subCategory}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 appearance-none"
-                        >
-                            <option value="" disabled>Select sub-category</option>
-                            <option value="Math Toys">Math Toys</option>
-                            <option value="Language Toys">Language Toys</option>
-                            <option value="Science Toys">Science Toys</option>
-                        </select>
-
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="price">Price:</label>
-                        <input
-                            type="text"
-                            id="price"
-                            name="price"
-                            value={toyData.price}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
-
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="rating">Rating:</label>
-                        <input
-                            type="text"
-                            id="rating"
-                            name="rating"
-                            value={toyData.rating}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
-
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="availableQuantity">Available Quantity:</label>
-                        <input
-                            type="text"
-                            id="availableQuantity"
-                            name="availableQuantity"
-                            value={toyData.availableQuantity}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        />
-
-                        <label className="text-gray-700 text-sm font-bold" htmlFor="description">Detail Description:</label>
-                        <textarea
-                            id="description"
-                            name="description"
-                            value={toyData.description}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                        ></textarea>
-                    </div>
-                </div>
-
-                <button
-                    type="submit"
-                    className="bg-[#09CCD0] hover:bg-[#3C404B] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            <div className="py-10 px-10 w-full lg:w-8/12 mx-auto  ">
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="p-7 bg-white rounded shadow-md"
                 >
-                    Submit
-                </button>
-            </form>
+                    <h1 className="text-3xl text-center">Add new toy</h1>
+                    <div className="divider"></div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div>
+                            <label htmlFor="toyName" className="block text-gray-700">
+                                Toy Name
+                            </label>
+                            <input
+                                type="text"
+                                id="toyName"
+                                placeholder="Toy Name"
+                                {...register("ToyName", { required: true, maxLength: 80 })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="subCategory" className="block text-gray-700">
+                                Sub Category
+                            </label>
+                            <select
+                                id="subCategory"
+                                {...register("SubCategory", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            >
+                                <option value="regular">Regular car</option>
+                                <option value="sports">Sports car</option>
+                                <option value="truck">Truck</option>
+                                {/* <option value="police">Police car</option> */}
+                            </select>
+                        </div>
+
+                        <div>
+                            <label htmlFor="price" className="block text-gray-700">
+                                Price
+                            </label>
+                            <input
+                                type="number"
+                                id="price"
+                                placeholder="Price"
+                                {...register("Price", { required: true, min: 0, maxLength: 100 })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="quantity" className="block text-gray-700">
+                                Available Quantity
+                            </label>
+                            <input
+                                type="number"
+                                id="quantity"
+                                placeholder="Available Quantity"
+                                {...register("AvailableQuantity", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="rating" className="block text-gray-700">
+                                Rating
+                            </label>
+                            <input
+                                type="text"
+                                id="rating"
+                                placeholder="Rating"
+                                {...register("Ratings", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="sellerName" className="block text-gray-700">
+                                Seller Name
+                            </label>
+                            <input
+                                type="text"
+                                id="sellerName"
+                                defaultValue={user?.displayName}
+                                placeholder="Seller Name"
+                                {...register("SellerName", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="sellerEmail" className="block w-full text-gray-700">
+                                Seller Email
+                            </label>
+                            <input
+                                type="text"
+                                id="sellerEmail"
+                                defaultValue={user?.email}
+                                placeholder="Seller Email"
+                                {...register("email", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="pictureUrl" className="block text-gray-700">
+                                Picture URL
+                            </label>
+                            <input
+                                type="url"
+                                id="pictureUrl"
+                                placeholder="Picture URL"
+                                {...register("Pictureurl", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label htmlFor="details" className="block text-gray-700">
+                                Details
+                            </label>
+                            <textarea
+                                id="details"
+                                {...register("Details", { required: true })}
+                                className="w-full border border-gray-300 rounded px-3 py-2 mt-1"
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <input
+                            type="submit"
+                            value="Add Toy"
+                            className=" w-full bg-[#80DFEB]  hover:bg-[#838F97] text-white font-bold py-2 px-4 rounded mx-auto mt-4"
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,39 +3,46 @@ import logo from '../../../assets/Logo.png'
 
 const Footer = () => {
     return (
-        <footer
-            className="footer p-10 bg-[#E8F6F7] text-base-content"
-            style={{
-                backgroundImage: 'url(https://i.ibb.co/bbQQ1Dg/footer.jpg)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}
-        >
-            <div >
-                <img src={logo} alt="" />
-                <p>ToyLand Industries Ltd.<br />Providing reliable tech since </p>
-            </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <Link to='/' className="link link-hover">Home</Link>
-                <Link to='/alltoys' className="link link-hover">All Toy</Link>
-                <Link to='/mytoys' className="link link-hover">My Toy</Link>
-                <Link to='/addatoy' className="link link-hover">Add A Toy</Link>
+        <div>
+            <footer
+                className="footer p-10 bg-[#E8F6F7] text-base-content"
+                style={{
+                    backgroundImage: 'url(https://i.ibb.co/bbQQ1Dg/footer.jpg)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            >
+                <div >
+                    <img src={logo} alt="" />
+                    <p>ToyLand Industries Ltd.<br />Providing reliable tech since </p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Home</Link>
+                    <Link to='/alltoys' className="link link-hover">All Toy</Link>
+                    <Link to='/mytoys' className="link link-hover">My Toy</Link>
+                    <Link to='/addatoy' className="link link-hover">Add A Toy</Link>
 
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <Link to='/' className="link link-hover">About us</Link>
-                <Link to='/blogs' className="link link-hover">Blog</Link>
+                </div>
+                <div>
+                    <span className="footer-title">CONTACT INFORMATION</span>
+                    <Link to='/' className="link link-hover">Kuril,Dhaka-1229</Link>
+                    <Link to='/blogs' className="link link-hover">018 456 779</Link>
+                    <Link to='/blogs' className="link link-hover">toyland@gmail.com</Link>
 
+                </div>
+                <div>
+                    <span className="footer-title">SOCIAL MEDIA</span>
+                    <Link to='/' className="link link-hover">Facebook</Link>
+                    <Link to='/' className="link link-hover">LinkIn</Link>
+                    <Link to='/' className="link link-hover">Twitter</Link>
+                </div>
+
+            </footer>
+            <div className="bg-[#80DFEB] footer footer-center p-4 font-bold text-[#838F97]-content">
+                <p>Copyright © 2023 - All right reserved by ToyLand  Industries Ltd</p>
             </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <Link to='/' className="link link-hover">Terms of use</Link>
-                <Link to='/' className="link link-hover">Privacy policy</Link>
-                <Link to='/' className="link link-hover">Cookie policy</Link>
-            </div>
-        </footer>
+        </div>
     );
 };
 
